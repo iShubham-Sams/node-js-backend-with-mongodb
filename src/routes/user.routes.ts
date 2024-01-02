68a87de6fca7e8a4f5dc5db8bb29899771eee198
bhandari_shubham <shubhambhandari12345hb@gmail.com>
2024-01-02 14:43:40 +0530

    add new routes for user controller

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,17 @@
 import { upload } from "../middlewares/multer.middleware.js";
 import zodValidate from "../middlewares/zodValidation.middleware.js";
 import {
+  changeUserPassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  getWatchHistory,
   logOutUser,
   loginUser,
   refreshAccessToken,
   registerUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
 } from "../controllers/user.controller.js";
 import { Router } from "express";
 import {
@@ -28,5 +35,18 @@ router.route("/login").post(zodValidate(loginUserZodSchema), loginUser);
 
 router.route("/logout").post(verifyJwt, logOutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJwt, changeUserPassword);
+router.route("/current-user").get(verifyJwt, getCurrentUser);
+router.route("/update-account").patch(verifyJwt, updateAccountDetails);
+
+router
+  .route("/avatar")
+  .patch(verifyJwt, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/cover-image")
+  .patch(verifyJwt, upload.single("coverImage"), updateUserCoverImage);
+
+router.route("/c/:username").get(verifyJwt, getUserChannelProfile);
+router.route("/history").get(verifyJwt, getWatchHistory);
 
 export default router;

97c600bb50b7855f93d64e555b3b3597d5688c82
bhandari_shubham <shubhambhandari12345hb@gmail.com>
2024-01-16 22:56:51 +0530

    get user tweet done

--- src/controllers/tweet.controller.ts
+++ src/controllers/tweet.controller.ts
@@ -1,4 +1,4 @@
-import { Response } from "express";
+import { Response, Request } from "express";
 import mongoose, { isValidObjectId } from "mongoose";
 import { Tweet } from "../models/tweet.model.js";
 import { User } from "../models/user.model.js";
@@ -18,15 +18,51 @@ const createTweet = asyncHandler(async (req: CustomRequest, res: Response) => {
     owner: req.user._id,
   });
   if (!tweetCreate) {
-    res.send(ApiError.ServerError("Something went Wrong "));
+    res.send(ApiError.ServerError("Something went Wrong"));
   }
   res.send(
     new ApiResponse(HttpStatusCode.CREATED, { message: "Tweet created" })
   );
 });
 
-const getUserTweets = asyncHandler(async (req, res) => {
-  // TODO: get user tweets
+const getUserTweets = asyncHandler(async (req: Request, res: Response) => {
+  try {
+    const id = req.params.userId;
+    // approach one
+    // const user = await User.findById(id);
+    // if (!user) {
+    //   throw new ApiError(
+    //     "User doesn't exist",
+    //     HttpStatusCode.BAD_REQUEST,
+    //     "Bad request"
+    //   );
+    // }
+    // const tweet = await Tweet.find({ owner: user._id });
+    // approach second
+    const valid = isValidObjectId(id);
+    if (!valid) {
+      throw new ApiError(
+        "User doesn't exist",
+        HttpStatusCode.BAD_REQUEST,
+        "Bad request"
+      );
+    }
+    const tweet = await Tweet.find({ owner: id });
+    res.send(
+      new ApiResponse(HttpStatusCode.OK, {
+        message: "Tweet Fetched",
+        data: tweet,
+      })
+    );
+  } catch (error) {
+    res.send(
+      new ApiError(
+        "User doesn't exist",
+        HttpStatusCode.BAD_REQUEST,
+        "Bad request"
+      )
+    );
+  }
 });
 
 const updateTweet = asyncHandler(async (req, res) => {
